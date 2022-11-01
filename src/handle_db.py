68e19c29e3d07c7c33111eedeb2af465551f90b4
cStaples95 <74618860+cStaples95@users.noreply.github.com>
2022-11-01 16:35:03 -0400

    Fixed bug, array now contains all elements from column

--- src/handle_db.py
+++ src/handle_db.py
@@ -3,21 +3,27 @@
 # Version .2
 import sqlite3
 
+
 class HandleDB():
     def __init__(self):
-    
-            # Connect to the database
-            self.conn = sqlite3.connect('Cost_DB.DB')
-            # Create a cursor to send SQL commands to database
-            self.cursor = self.conn.cursor()
+
+        # Connect to the database
+        self.conn = sqlite3.connect('Cost_DB.DB')
+        # Create a cursor to send SQL commands to database
+        self.cursor = self.conn.cursor()
 
     def pull_vuln_data(self):
-        # Pull the vulnerability data from the database
+        # Pull the vulnerability name from the database
         vuln_data = []
-        for row in self.cursor.execute("SELECT vulnerability FROM Vul_Cost"):
-            vuln_data = self.cursor.fetchall()
+        self.cursor.execute("SELECT vulnerability FROM Vul_Cost")
+        for row in self.cursor:
+            vuln_data.insert(0, row[0])
         return vuln_data
-        conn.close()
+
 
 HandleDB = HandleDB()
-print(HandleDB.pull_vuln_data())
+# print(HandleDB.pull_vuln_data())
+data = []
+data = HandleDB.pull_vuln_data()
+for x in data:
+    print(x)
