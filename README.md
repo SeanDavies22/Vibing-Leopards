5b392fcee43f46b43d7d42090e6a2c0d3e15a4ac
cStaples95 <74618860+cStaples95@users.noreply.github.com>
2022-11-01 19:05:33 -0400

    Added editing cost field in database, added extra test commands

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 -This has been made into a class to house the funtionally of dealing with database
 - It will return return columns from the database as array's
 - currently only returns the vulnerability names from said column in database
+- Added editing the cost field of vulnerabilities in the DB, the v ulnerability names must be exact for it to work
--- src/handle_db.py
+++ src/handle_db.py
@@ -12,6 +12,10 @@ class HandleDB():
         # Create a cursor to send SQL commands to database
         self.cursor = self.conn.cursor()
 
+    # On deletion of object, close the connection to the database
+    def __del__(self):
+        self.conn.close()
+
     def pull_vuln_data(self):
         # Pull the vulnerability name from the database
         vuln_data = []
@@ -20,6 +24,20 @@ class HandleDB():
             vuln_data.insert(0, row[0])
         return vuln_data
 
+    def pull_risk_data(self):
+        # Pull the vulnerability risk from the database
+        risk_data = []
+        self.cursor.execute("SELECT risk_level FROM Vul_Cost")
+        for row in self.cursor:
+            risk_data.insert(0, row[0])
+        return risk_data
+
+    def push_cost_data(self, vuln, cost):
+        # Push the vulnerability cost to the database
+        self.cursor.execute(
+            "UPDATE Vul_Cost SET cost_hour = ? WHERE vulnerability = ?", (cost, vuln))
+        self.conn.commit()
+
 
 HandleDB = HandleDB()
 # print(HandleDB.pull_vuln_data())
@@ -27,3 +45,17 @@ data = []
 data = HandleDB.pull_vuln_data()
 for x in data:
     print(x)
+data = HandleDB.pull_risk_data()
+for x in data:
+    print(x)
+for row in HandleDB.cursor.execute("SELECT * FROM Vul_Cost"):
+    print(row)
+input("Press Enter to continue...")
+cost = input("Enter the cost: ")
+vuln = input("Enter the vulnerability: ")
+
+HandleDB.push_cost_data(vuln, cost)
+for row in HandleDB.cursor.execute("SELECT * FROM Vul_Cost"):
+    print(row)
+
+del HandleDB
