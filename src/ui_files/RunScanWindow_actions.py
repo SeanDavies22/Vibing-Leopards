21f814e2879edf42d1c5a4fa28a56e6b818a49fa
cStaples95 <74618860+cStaples95@users.noreply.github.com>
2022-11-09 19:33:11 -0500

    New database that includes all cve ids from website, also update

--- src/ui_files/RunScanWindow_actions.py
+++ src/ui_files/RunScanWindow_actions.py
@@ -3,12 +3,13 @@
 # Edited: 11/8/22
 
 # This file is an action handler for the Run Analysis window of the
-# Cost Benefit Analysis Tool. 
+# Cost Benefit Analysis Tool.
 
 from RunScanWindow import Ui_RunScanWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 import xml.etree.ElementTree as ET
 from handle_db import *
+
 
 class RunScanGUI(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
@@ -28,14 +29,13 @@ class RunScanGUI(QtWidgets.QMainWindow):
 
     def parse_xml(self, filePath):
         file = open(filePath, "r")
-        
-        cveid_list = []
+
+        database_cost = []
         db_handler = HandleDB()
 
         self.rsg = RunScanGUI()
         self.rsg.show_gui()
         # Pull cve ids from the database, store into the array
-        cveid_list = db_handler.pull_cve_id()
 
         # This is suppose to create the tree thing so
         # that we can parse the nessus file
@@ -46,9 +46,10 @@ class RunScanGUI(QtWidgets.QMainWindow):
         # Prints out matches found in the nessus file
         msg = QtWidgets.QMessageBox()
         for nessus_cve in root.iter('cve'):
-            for db_cve in cveid_list:
-                if nessus_cve.text.__contains__(db_cve):
-                    msg.setText("Match found: " + db_handler.pull_description(db_cve))
-                    msg.setWindowTitle("File Explorer")
-                    msg.setIcon(QtWidgets.QMessageBox.Information)  
-                    x = msg.exec_()
+            database_cost = db_handler.pull_cost_data(nessus_cve.text)
+            database_cost = str(database_cost)
+            msg.setText("Match found: " + nessus_cve.text + " " +
+                        database_cost[0] + " " + database_cost[1])
+            msg.setWindowTitle("File Explorer")
+            msg.setIcon(QtWidgets.QMessageBox.Information)
+            x = msg.exec_()
